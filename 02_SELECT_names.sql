9385950974168a51cf229a6c96ca71500bb5f7d7
Karmaester <karmaester@LAPTOP-P84UAG4M.localdomain>
2020-10-28 15:00:52 -0300

    Add first four quizes

---
+++ 02_SELECT_names.sql
@@ -0,0 +1,34 @@
+SELECT name FROM world
+  WHERE name LIKE 'B%'
+
+SELECT name FROM world
+  WHERE name LIKE '%y'
+
+SELECT name FROM world
+  WHERE name LIKE '%x%'
+
+SELECT name FROM world
+  WHERE name LIKE '%land'
+
+SELECT name FROM world
+  WHERE name LIKE 'C%ia'
+
+SELECT name FROM world
+  WHERE name LIKE '%oo%'
+
+SELECT name FROM world
+  WHERE name LIKE '%a%a%a%'
+
+SELECT name FROM world
+ WHERE name LIKE '_t%'
+ORDER BY name
+
+SELECT name FROM world
+ WHERE name LIKE '%o__o%'
+
+SELECT name FROM world
+ WHERE name LIKE '____'
+
+SELECT name
+  FROM world
+ WHERE name = capital 
